473279f158bad7e1c76ec1da7561e01fa4508ffc
HereAfteRain <max799198985@163.com>
2024-05-24 20:42:42 +0800

    5.24

---
+++ 3DGSRender/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <pcl/point_cloud.h>
+#include <pcl/octree/octree.h>
+#include <boost/thread/thread.hpp>
+#include <pcl/visualization/pcl_visualizer.h>
+
+
+int main(int argc, char** argv) {
+	return 0;
+}
